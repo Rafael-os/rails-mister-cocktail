99c3652b97cb9a6bedebed5eec5b941e55da11e4
rafael osawa <osawa.rafael@gmail.com>
2020-05-29 00:51:19 +0300

    Need to continue

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,19 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  	@cocktail = Cocktail.find(params[:id])
+  	@dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+	@cocktail = Cocktial.new(cocktail_params)
+	@cocktail.save
+  end
+end
